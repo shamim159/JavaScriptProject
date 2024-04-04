5c7d1256cabda03a94013e88edbb4b0ef470243c
shamim159 <shamin_ali@hotmail.co.uk>
2024-04-04 23:56:38 +0100

    get computer choice function complete: created a function which matches 0,1,2 and assigns the values to my array in order. can be viewed via console log

---
+++ main.js
@@ -0,0 +1,16 @@
+//function to generate computers random
+function getComputerChoice() {
+  const  myArray=["Rock" , "Paper", "Scissors"];
+  const randNum = Math.floor(Math.random() * myArray.length);//my array.length = 3 values assigned to 0,1,2
+  console.log(randNum, myArray[randNum]);
+}  
+
+getComputerChoice()
+  
+
+
+
+
+
+//score track variables
+
