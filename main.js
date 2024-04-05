a6c29ba0bd3f47ba334fcc771e1e98a327f2a109
shamim159 <shamin_ali@hotmail.co.uk>
2024-04-05 12:29:33 +0100

    added if statement and stored player value as scissors.

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 //function to generate computers random
 function getComputerChoice() {
-  const  myArray=["Rock" , "Paper", "Scissors"];
+  const  myArray=["Rock", "Scissors", "Paper" ];
   const randNum = Math.floor(Math.random() * myArray.length);//my array.length = 3 values assigned to 0,1,2
-  console.log(randNum, myArray[randNum]);
+  return(randNum, myArray[randNum]);
 }  
 
-getComputerChoice()
+function getPlayerChoice() {
+  prompt("please input either: Rock, Paper or Scissors")
+}
+
+//conditions
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock ";
+  } else if (playerSelection === "Rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+    return "You Lose! Scissors beats paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock Beats Scissors";
+  } else if (playerSelection == computerSelection) {
+    return "Its a draw";
+  }else{
+    return "Invalid Choice, try again!"
+  } 
+}
+
+const playerSelection = "Scissors"// selected a random array. seems to work on th console
+
+const computerSelection = getComputerChoice();//runs this function
+
+
+console.log(playRound(playerSelection, computerSelection)); //intiates on the console with player selection scissors
+
+
+
   
 
 
