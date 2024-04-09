23216690cafa49514cea09e8c41e87d10a451b94
shamim159 <shamin_ali@hotmail.co.uk>
2024-04-09 16:15:05 +0100

    logic updated with concat

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 //function to generate computers random
+let computerScore = 0;
+let playerScore = 0;
+const  myArray=["Rock", "Scissors", "Paper" ];
+
 function getComputerChoice() {
-  const  myArray=["Rock", "Scissors", "Paper" ];
-  const randNum = Math.floor(Math.random() * myArray.length);//my array.length = 3 values assigned to 0,1,2
-  return(randNum, myArray[randNum]);
+  const randNum = myArray[Math.floor(Math.random() * myArray.length)];//my array.length = 3 values assigned to 0,1,2
+  return (randNum)  
+
+
 }  
+
 
 function getPlayerChoice() {
   prompt("please input either: Rock, Paper or Scissors")
@@ -21,28 +27,28 @@ if (playerSelection === computerSelection) {
 else{
   switch(playerSelection){//changed to switch method for cleaner code
     case "Rock":
-      result = (computerSelection === "Scissors") ? "You Win!" : "You Lose!";
+      result = (computerSelection === "Scissors") ? `You Win! your ${playerSelection} beats opponents ${computerSelection}`    : `You Lose! opponents ${computerSelection} beats your ${playerSelection}` ;//updated logic as console log throw different results from the randNum.
       break;
-    case "Paper":
-      result = (computerSelection === "Rock") ? "You Win!" : "You Lose!";
-      break;
-    case "Scissors":
-      result = (computerSelection === "Paper") ? "You Win!" : "You Lose!";
-      break;
-  }
-}
-console.log(result)
-}
+      case "Paper":
+        result = (computerSelection === "Rock") ? `You Win! your ${playerSelection} beats opponents ${computerSelection}`    : `You Lose! opponents ${computerSelection} beats your ${playerSelection}` ;
+        break;
+        case "Scissors":
+          result = (computerSelection === "Paper") ? `You Win! your ${playerSelection} beats opponents ${computerSelection}`    : `You Lose! opponents ${computerSelection} beats your ${playerSelection}` ;
+          break;
+          
+        }
+        
+      }
+      console.log(result)
+      switch(result){
+        case "You Win! ":
+          playerScore ++;
+          break;
+          case "You Lose! ":
+            computerScore ++;
+            break;
+          }
+          
+        }
 
-playRound(); //intiates on the console with player selection scissors
-
-
-
-  
-
-
-
-
-
-//score track variables
-
+        playRound();
