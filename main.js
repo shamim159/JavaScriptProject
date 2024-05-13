5e0c73acabc7cff7870a8664ef9a4dd600f81f0c
shamim159 <shamin_ali@hotmail.co.uk>
2024-05-13 14:57:48 +0100

    Removed prompts and added user buttons

--- main.js
+++ main.js
@@ -1,34 +1,67 @@
-//function to generate computers random
-const  myArray=["rock", "scissors", "paper" ];
+const body = document.querySelector('body');//selects the target
+const rockButton = document.createElement('button');
+const paperButton = document.createElement('button');
+const scissorsButton = document.createElement('button');
+
+rockButton.textContent = "rock";
+paperButton.textContent = "paper";
+scissorsButton.textContent = "scissors";
 
 
 
-
+body.appendChild(rockButton);
+body.appendChild(paperButton);
+body.appendChild(scissorsButton);
 
 function getComputerChoice() {//computer score generator
+  const  myArray=["rock", "scissors", "paper" ];
   const randNum = myArray[Math.floor(Math.random() * myArray.length)];//my array.length = 3 values assigned to 0,1,2
   return (randNum);  
 };  
 
-function getPlayerChoice() {// get player input and conditioned so that a correct value is returned otherwise it will keep prompting.
+function getPlayerChoice() {
   let validatedInput = false;
     while(validatedInput == false){
-      const pick = prompt("please input either: Rock, Paper or Scissors")
+      const pick = rockButton || paperButton || scissorsButton;
       if(pick == null) {
-        continue;// will keep prompting if unmatched values are inputed
+        getPlayerChoice();// will keep prompting if unmatched values are inputed
       }
-      const pickInLower = pick.toLowerCase();//function to change input to all lowercase so that the above prompt input is made easier for human
-      if (myArray.includes(pickInLower)) {
-        validatedInput = true;
-        return pickInLower;
-      }
-    }
-};
+}
+}
+
+rockButton.addEventListener("click", (e) => {
+  getPlayerChoice = e.target.innerHTML;
+  game();    
+});
+
+paperButton.addEventListener("click", (e) => {
+  getPlayerChoice = e.target.innerHTML;
+  game();    
+});
+
+scissorsButton.addEventListener("click", (e) => {
+  getPlayerChoice = e.target.innerHTML;
+  game();    
+});
+
+  
+
+
+
+//const playerSelection = getPlayerChoice();//runs player choice and computer choice below and stores in variables.
+//const computerSelection = getComputerChoice();
+
+
+//function to generate computers random
+
+
+
+
 
 
 function playRound(playerSelection, computerSelection) {//game logic using ternary operator
   let result = ""
-//conditions
+  //conditions
 if (playerSelection == computerSelection) {
   result = "Its a Draw!";
 }else{
@@ -36,30 +69,27 @@ if (playerSelection == computerSelection) {
     case "rock":
       result = (computerSelection == "scissors") ? "You Win! "  : "You Lose! "  ;//updated logic as console log throw different results from the randNum.
       break;
-    case "paper":
-      result = (computerSelection == "rock") ? "You Win! "    : "You Lose! "  ;
-      break;
-    case "scissors":
-      result = (computerSelection == "paper") ? "You Win! "  : "You Lose! "  ;
-      break;   
-  }  
-}
-  return result;
-};
-
+      case "paper":
+        result = (computerSelection == "rock") ? "You Win! "    : "You Lose! "  ;
+        break;
+        case "scissors":
+          result = (computerSelection == "paper") ? "You Win! "  : "You Lose! "  ;
+          break;   
+        }  
+      }
+      return result;
+    };
+    
+    
     
 
-
     
-
-
-function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;
-    console.log("Hello")
-    for (let i = 0; i < 5; i++){// 2.loops the below five times
-      const playerSelection = getPlayerChoice();//runs player choice and computer choice below and stores in variables.
-      const computerSelection = getComputerChoice();
+    function game() {
+      console.log("WELCOME to RPS")
+        const playerSelection = getPlayerChoice;//runs player choice and computer choice below and stores in variables.
+        const computerSelection = getComputerChoice();
       console.log(`player: ${playerSelection} --VS-- computer: ${computerSelection}`);// logs the scores
     
       if(playRound(playerSelection, computerSelection) == "You Win! ") {
@@ -75,9 +105,23 @@ function game() {
         console.log("Tie breaker!");
       }
       console.log("---------------------");
+      console.log(scorePlayer)
+      console.log(scoreComputer)
+      
+      if(scoreComputer === 5){
+        console.log("GAME OVER : COMPUTER WINS")
+      }else if(scorePlayer === 5){
+        console.log("WINNER WINNER CHICKEN DINNER!")
+      }else{
+        "WE HAVE A TIE!"
+      }
+      
       
     }
-    console.log("Game Over")
+    
+    
+    
+    /*console.log("Game Over")
     if(scorePlayer > scoreComputer) {
       console.log("WINNER WINNER CHICKEN DINNER!");
     }else if(scorePlayer < scoreComputer) {
@@ -87,5 +131,5 @@ function game() {
     }
     console.log(`player: ${scorePlayer}`)
     console.log(`computer: ${scoreComputer}`)
-};
-game();// 1. runs the game function
+};*/
+//game();// 1. runs the game function
